a2ffa72066e9b7ce0536d2cfdb9dd1d46676747c
nurriol2 <nurriol2@illinois.edu>
2019-02-08 14:24:59 -0600

    moved to channelAssignmentNaive dir

--- alpha/transChannel.py
+++
@@ -1,7 +0,0 @@
-def peaks(channelDictionary):
-
-    maximumCount = max(channelDictionary.values())
-
-    channelNumber = [channel for channel, count in channelDictionary.items() if count == maximumCount]
-
-    return channelNumber
