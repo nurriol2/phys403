a310a7c1bac65ccffcfb5701b7bc3b3fcb89d85d
nurriol2 <nurriol2@illinois.edu>
2019-02-08 18:37:48 -0600

    docstring explains output

--- alpha/noiseWalk.py
+++ alpha/noiseWalk.py
@@ -2,20 +2,25 @@
 
 def noise_walk():
 
+    '''
+    noise_walk applies noise_verify to all files within a given directory
+
+    @input:
+    None
+
+    @output:
+    noise_walk prints basic data to console (SEE:  noise_verify).
+    noise_walk writes this data to a .txt file. (IMPLEMENTATION TO COME)
+    '''
+
     import os
     from noiseVerify import noise_verify
     import logging
     logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
 
-    cwd = os.getcwd()
-
     constant_pressure_directory = input('Directory to walk:  ')
 
     for root, dirs, files in os.walk(constant_pressure_directory):
-        #print('root:  %s' %root)
-        #print('dirs:  %s' %dirs)
-        #print('files:  %s' %files)
-
         for filename in files:
             noise_verify(constant_pressure_directory, filename)
 
