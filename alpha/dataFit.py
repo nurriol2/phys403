30b0faa56a2a6134a38b8fe151fcd3b85ebd9214
nurriol2 <nurriol2@illinois.edu>
2019-02-10 18:45:19 -0600

    formats x and y data for histogram

--- alpha/dataFit.py
+++ alpha/dataFit.py
@@ -9,12 +9,19 @@ def fit_to_data():
     import logging
     logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
 
+    #ask user for filename
+    user_file = input('Enter filename:  ')
 
-    cwd = os.getcwd()
-    #test pulse data is collected specifically for channel calibration
-    dir_name = '/testPulseData/'
+    #format user_file for file_path
+    file_name = '/' + user_file
 
-    file_path = cwd + dir_name
+    #calibration data is located in testPulseData directory
+    test_pulse_dir = os.path.abspath('testPulseData')
+
+    #create full path to file
+    file_path = test_pulse_dir + file_name
+
+    logging.debug('file_path = %s' %file_path)
 
     file = open(file_path)
     raw_data = file.readlines()
@@ -22,10 +29,15 @@ def fit_to_data():
 
     count_data = raw_data[12:len(raw_data) - 1]
 
-    for dataPoint in count_data:
-        dataPoint = int(dataPoint)
-    logging.debug(count_data)
+    #y data of histogram
+    count_array = np.array(count_data, dtype = int)
+    logging.debug('count_data_array:  %s' %count_data_array)
 
+    #x data of histogram
+    bins_array = np.linspace(1, 1024, 1024)
+    logging.debug(len(bins))
+
+    
 
 
 
